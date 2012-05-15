9dcd8ce8b9fd26b56900602a58cdddde653c71dd
randax <oyvind.randa@gmail.com>
2012-05-15 18:06:24 +0200

    Hente spiller navn

--- test/GetMatchesTest.java
+++ test/GetMatchesTest.java
@@ -1,35 +1,37 @@
+import models.Match;
+import models.Team;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.junit.*;
+import org.junit.Test;
+import play.test.UnitTest;
 
 import java.io.IOException;
-import java.util.*;
-import play.test.*;
-import models.*;
 
 public class GetMatchesTest extends UnitTest {
 
     @Test
     public void doIt() throws IOException {
 
-      Document doc = Jsoup.connect("http://www.altomfotball.no/element.do?cmd=tournament&tournamentId=6270").get();
-      Elements elements = doc.select(".sd_fixtures tr");
+        Document doc = Jsoup.connect("http://www.altomfotball.no/element.do?cmd=tournament&tournamentId=6270").get();
+        Elements elements = doc.select(".sd_fixtures tr");
 
-      for (Element element : elements) {
-        String homeTeamName = element.select(".sd_fixtures_home_flag a").text();
-        String awayTeamName = element.select(".sd_fixtures_away_flag a").text();
+        for (Element element : elements) {
+            String homeTeamName = element.select(".sd_fixtures_home_flag a").text();
+            String awayTeamName = element.select(".sd_fixtures_away_flag a").text();
+            String groupId = element.select(".sd_fixtures_tournament span").text();
 
-        Team homeTeam = Team.getTeam(homeTeamName);
-        Team awayTeam = Team.getTeam(awayTeamName);
+            System.out.println(groupId);
+            Team homeTeam = Team.getTeam(homeTeamName);
+            Team awayTeam = Team.getTeam(awayTeamName);
 
-        Match match = new Match(homeTeam, awayTeam);
-        match.save();
+            Match match = new Match(homeTeam, awayTeam);
+            match.save();
 
-        System.out.println(match.toString());
+            System.out.println(match.toString());
 
-      }
+        }
 
     }
 
