b5e96cf233f8d76cc3bbb38bf11138b9b4bdcc09
randax <oyvind.randa@gmail.com>
2012-05-20 22:15:07 +0200

    Hente spiller navn

--- test/GetPlayersTest.java
+++ test/GetPlayersTest.java
@@ -1,6 +1,3 @@
-import models.Match;
-import models.Players;
-import models.Team;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -14,21 +11,57 @@ public class GetPlayersTest extends UnitTest {
 
     @Test
     public void doIt() throws IOException {
-      String url;
-      int []teamid = {56370, 58837,35, 39, 43, 47, 49, 66, 95, 109, 110, 64, 57451, 122, 127, 57166};
-      for(int i=0; i<teamid.length; i++)  {
-      url ="http://www.uefa.com/uefaeuro/season=2012/teams/team=" + teamid[i] + "/squad/index.html";
-      System.out.println(url);
-      Document doc = Jsoup.connect(url).get();
-      Elements elements = doc.select(".squadlist");
+        String url;
+        int[] teamid = {56370, 58837, 35, 39, 43, 47, 49, 66, 95, 109, 110, 64, 57451, 122, 127, 57166};
+        for (int i = 0; i < teamid.length; i++) {
+            url = "http://www.uefa.com/uefaeuro/season=2012/teams/team=" + teamid[i] + "/squad/index.html";
+            // System.out.println(url);
+            Document doc = Jsoup.connect(url).get();
+            Elements elements = doc.select(".squadlist");
 
-      for (Element element : elements) {
-        String PlayerName = element.select(".medTitle a").text();
-        System.out.println(PlayerName);
+            for (Element element : elements) {
+                Elements players = element.select(".medTitle a");
+                for (Element player : players) {
 
-      }
-      }
+
+                    String PlayerURL = player.attr("href");
+                    Document Doc = Jsoup.parse(PlayerURL);
+                    String PlayerID = Doc.body().text();
+                    int tempID = Integer.parseInt(PlayerID.split("=")[2].split("/")[0]);
+                    playerBio(tempID);
+                    //System.out.println("navn = " + PlayerName + "ID = " + tempID);
+
+
+                }
+            }
+        }
 
     }
+
+    public static void playerBio(int PlayerID) throws IOException {
+
+        String url = "http://www.uefa.com/uefaeuro/season=2012/teams/player=" + PlayerID + "/index.html";
+        Document doc = Jsoup.connect(url).get();
+        Elements elements = doc.select(".infoBox");
+
+        for (Element element : elements) {
+
+            Elements Bios = element.select(".innerText li");
+            for (Element Bio : Bios) {
+                String name = Bios.get(0).text().split(": ")[1];
+                String Posision = Bios.get(1).text().split(": ")[1];
+                String Nationality = Bios.get(3).text().split(": ")[1];
+                String Club = Bios.get(4).text().split(": ")[1];
+
+                System.out.println(name);
+                System.out.println(Posision);
+                System.out.println(Nationality);
+                System.out.println(Club);
+
+            }
+
+        }
+    }
+
 
 }
